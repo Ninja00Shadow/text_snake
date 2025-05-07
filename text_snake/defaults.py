17562b98608a71d3c39705bb6417ec6dc27451bd
Dawid <mrshadow813@gmail.com>
2025-05-07 10:23:09 +0200

    Add defaults management

---
+++ text_snake/defaults.py
@@ -0,0 +1,29 @@
+import pathlib
+
+BASE = pathlib.Path(__file__).parent
+file_path = BASE / "defaults.txt"
+
+def read_defaults():
+    """Read the default values from the file."""
+    defaults = {}
+    try:
+        with open(file_path, "r") as file:
+            for line in file:
+                key, value = line.strip().split("=")
+                defaults[key] = int(value)
+    except FileNotFoundError:
+        defaults = {
+            "speed": 30,
+            "length": 3
+        }
+        write_defaults(defaults)
+    return defaults
+
+def write_defaults(defaults:dict):
+    """Write the default values to the file."""
+    with open(file_path, "w") as file:
+        for key, value in defaults.items():
+            file.write(f"{key}={value}\n")
+
+if __name__ == "__main__":
+    print(read_defaults())
--- text_snake/main.py
+++ text_snake/main.py
@@ -1,8 +1,10 @@
 import argparse
 
+from text_snake.defaults import read_defaults, write_defaults
 from text_snake.engine import GameEngine
 from text_snake.scores import display_scores, clear_scores
 
+defaults = read_defaults()
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -12,8 +14,8 @@ def parse_args():
     parser.add_argument(
         "--speed",
         type=int,
-        default=30,
-        help="Game speed in frames per second (default: 30)"
+        default=defaults["speed"],
+        help="Game speed (default: 30)",
     )
 
     subparsers = parser.add_subparsers(title="commands", dest="command")
@@ -30,6 +32,32 @@ def parse_args():
         action="store_true",
         help="Clear all scores"
     )
+    # scores_parser.add_argument(
+    #     "-n",
+    #     "--number",
+    #     type=int,
+    #     default=10,
+    #     help="Number of stored scores (default: 10)"
+    # )
+
+    defaults_parser = subparsers.add_parser(
+        "defaults",
+        help="Change default game values",
+        description="Change default game values. Without arguments, it will display the current defaults.",
+    )
+
+    defaults_parser.add_argument(
+        "-s",
+        "--speed",
+        type=int,
+        help="Set the default game speed (min: 1)",
+    )
+    # defaults_parser.add_argument(
+    #     "-l",
+    #     "--length",
+    #     type=int,
+    #     help="Set the default snake length (min: 1)",
+    # )
 
     return parser.parse_args()
 
@@ -38,6 +66,26 @@ def start_game(args):
     game = GameEngine(fps=args.speed)
     game.run()
 
+def handle_defaults(args):
+    if args.speed is None and args.length is None:
+        print("Current defaults:")
+        print(f"Speed: {defaults['speed']}")
+        print(f"Length: {defaults['length']}")
+        return
+
+    if args.speed is not None:
+        if args.speed < 1:
+            print("Speed must be at least 1.")
+            return
+        defaults["speed"] = args.speed
+    if args.length is not None:
+        if args.length < 1:
+            print("Length must be at least 1.")
+            return
+        defaults["length"] = args.length
+
+    write_defaults(defaults)
+    return
 
 def main():
     args = parse_args()
@@ -47,6 +95,10 @@ def main():
         return
     if args.command == "scores" and args.clear:
         clear_scores()
+        return
+
+    if args.command == "defaults":
+        handle_defaults(args)
         return
 
     start_game(args)
